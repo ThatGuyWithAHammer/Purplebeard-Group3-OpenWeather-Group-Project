0dc4dd640779e5262689b80b00bfee49cb0f21ec
Enrico Carnevale <shuzolost@gmail.com>
2023-05-16 19:29:01 +0100

    Added actual date to date box, fixed API key

--- enrico-barebones/script.js
+++ enrico-barebones/script.js
@@ -1,8 +1,9 @@
 function getForecast() {
+  document;
   let location = document.getElementById("location").value;
 
   // Make a request to OpenWeatherMap API
-  let apiKey = "YOUR_API_KEY"; // Replace with your OpenWeatherMap API key
+  let apiKey = "26e2719dcbe0b94ca1bc10094c6ea568"; // Replace with your OpenWeatherMap API key
   let url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=${apiKey}`;
 
   fetch(url)
@@ -17,8 +18,11 @@ function getForecast() {
       );
       forecast.forEach((item) => {
         let date = new Date(item.dt_txt);
-        let dayOfWeek = date.toLocaleDateString("en-US", {
-          weekday: "long",
+        let dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" });
+        let dateString = date.toLocaleDateString("en-US", {
+          month: "short",
+          day: "numeric",
+          year: "numeric",
         });
         let temperature = Math.round(item.main.temp);
         let weatherDescription = item.weather[0].description;
@@ -27,10 +31,11 @@ function getForecast() {
         let dayDiv = document.createElement("div");
         dayDiv.className = "day";
         dayDiv.innerHTML = `
-          <h3>${dayOfWeek}</h3>
-          <p>${temperature}°C</p>
-          <p>${weatherDescription}</p>
-        `;
+            <h3>${dayOfWeek}</h3>
+            <p>${dateString}</p>
+            <p>${temperature}°C</p>
+            <p>${weatherDescription}</p>
+          `;
 
         // Add the day's forecast to the forecast container
         document.getElementById("forecast").appendChild(dayDiv);
